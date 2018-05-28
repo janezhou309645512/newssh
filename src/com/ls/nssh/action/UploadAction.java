5c6448c756ebf0860489b844298184a2ee257344
janezhou309645512 <309645512@qq.com>
2018-05-28 22:34:54 +0800

    2018-05-28 1
    上传与下载

--- src/com/ls/nssh/action/UploadAction.java
+++ src/com/ls/nssh/action/UploadAction.java
@@ -3,6 +3,7 @@ package com.ls.nssh.action;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 
 import org.apache.struts2.ServletActionContext;
 import org.springframework.context.annotation.Scope;
@@ -15,56 +16,29 @@ public class UploadAction extends BaseAction{
 	
 	
 	   
-	   private File file;  
-	      
-	    public File getFile() {  
-	        return file;  
-	    }  
-	  
-	    public void setFile(File file) {  
-	        this.file = file;  
-	    }  
-	  
-	    public String getFileFileName() {  
-	        return fileFileName;  
-	    }  
-	  
-	    public void setFileFileName(String fileFileName) {  
-	        this.fileFileName = fileFileName;  
-	    }  
-	  
-	    public String getFileContentType() {  
-	        return fileContentType;  
-	    }  
-	  
-	    public void setFileContentType(String fileContentType) {  
-	        this.fileContentType = fileContentType;  
-	    }  
-	  
-	    //文件名称  
-	    private String fileFileName;  
-	      
-	    //文件类型  
-	    private String fileContentType;  
-	    //注意：文件名称和文件类型的名称前缀必须相同，  
-	
+	private File file;//文件
+    private String fileFileName;//文件名称    
+    private String fileContentType; //文件类型   
+    private String filename;
+    private InputStream inputStream;
 	
 	public String uploadFile() throws Exception{ 
 		   
-		   //获取需要上传文件的文件路径  
-        File uploadFile=new File(ServletActionContext.getServletContext().getRealPath("D:/uploadFile"));  
+		//获取需要上传保存文件的文件路径  
+        File uploadFile=new File("D:/uploadFile");  
         //判断文件是否上传，如果上传的话将会创建该目录  
         if(!uploadFile.exists()){  
             uploadFile.mkdir(); //创建该目录  
         }  
-          
+        FileInputStream input=null;  
+        FileOutputStream out=null;
         //第一种文件上传的方法  
-        //声明文件输入流，为输入流指定文件路径  
-        FileInputStream input=new FileInputStream(file);  
-        //获取输出流，获取文件的文件地址及名称  
-        FileOutputStream out=new FileOutputStream(uploadFile + "\\" +fileFileName);  
+       
         try{
-      
+        	 //声明文件输入流，为输入流指定文件路径  
+            input=new FileInputStream(file);  
+           //获取输出流，获取文件的文件地址及名称  
+            out=new FileOutputStream(uploadFile + "\\" +fileFileName);  
             byte[] b=new byte[1024];//每次写入的大小  
             int i=0;  
             while((i=input.read(b))>0){  
@@ -78,6 +52,46 @@ public class UploadAction extends BaseAction{
         }  
 		
 		return  "OK";
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public String getFileFileName() {
+		return fileFileName;
+	}
+
+	public void setFileFileName(String fileFileName) {
+		this.fileFileName = fileFileName;
+	}
+
+	public String getFileContentType() {
+		return fileContentType;
+	}
+
+	public void setFileContentType(String fileContentType) {
+		this.fileContentType = fileContentType;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
 	}
 
 
